fe9e7f1849dc621dcb7068c9a2a753f498a3b803
mamamamamaa <maks.denishchuk@gmail.com>
2023-06-12 13:55:36 +0300

    update dog query params validation

--- src/modules/dog/dto/dog-query-params.dto.ts
+++ src/modules/dog/dto/dog-query-params.dto.ts
@@ -1,27 +1,37 @@
 import { IsEnum, IsInt, IsOptional, IsPositive, Min } from 'class-validator';
+import { Type } from 'class-transformer';
+import { OrderQueryParam } from '../../../types/dog';
+
+const attributeEnum = ['name', 'color', 'tail_length', 'weight'];
+const orderEnum = ['asc', 'desc'];
 
 export class DogQueryParamsDto {
   @IsOptional()
-  @IsEnum(['name', 'color', 'tail_length', 'weight'], {
-    message:
-      "attribute must be one of the following values: 'name', 'color', 'tail_length' or 'weight'",
+  @IsEnum(attributeEnum, {
+    message: `attribute must be one of the following values: ${attributeEnum.join(
+      ', ',
+    )}`,
   })
   attribute: string;
 
   @IsOptional()
-  @IsEnum(['asc', 'desc'], {
-    message: "order must be one of the following values: 'asc' or 'desc'",
+  @IsEnum(orderEnum, {
+    message: `order must be one of the following values: ${orderEnum.join(
+      ', ',
+    )}`,
   })
-  order: 'asc' | 'desc';
+  order: OrderQueryParam;
 
   @IsOptional()
   @IsInt()
   @Min(1)
+  @Type(() => Number)
   pageNumber: number;
 
   @IsOptional()
   @IsInt()
   @Min(1)
   @IsPositive()
+  @Type(() => Number)
   pageSize: number;
 }
---
+++ src/types/dog.ts
@@ -0,0 +1,1 @@
+export type OrderQueryParam = 'asc' | 'desc';
